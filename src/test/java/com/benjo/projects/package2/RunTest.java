0e994db281bec532ab85fb8004a064d6865efbc4
benjoibarra <benjoibarra20@gmail.com>
2020-02-17 19:40:18 +0800

    Set up project

--- src/test/java/com/benjo/projects/package2/RunTest.java
+++ src/test/java/com/benjo/projects/package2/RunTest.java
@@ -3,13 +3,14 @@ package com.benjo.projects.package2;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
 import net.thucydides.core.annotations.Steps;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import steps.SearchSteps;
 
 @RunWith(SerenityRunner.class)
-public class RunTest2 {
+public class RunTest {
 
     @Managed
     WebDriver driver;
